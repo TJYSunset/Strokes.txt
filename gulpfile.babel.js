cb912ea468bf849a321755fdc42ca9ceafe75312
Todd Justin York <tjysunset@outlook.com>
2018-08-13 09:12:53 +0800

    Fixed synchronicity for gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,40 +4,36 @@ import Console from "console";
 import * as rxjs from "rxjs";
 import { skipWhile, takeWhile, map, filter } from "rxjs/operators";
 
-gulp.task("generate-stub", function() {
+gulp.task("generate-stub", done => {
     const ids = fs.readFileSync("ext/IDS.txt", { encoding: "utf8" }).split(/\r\n|\n|\r/);
     const outputPath = "stub/Strokes.txt";
 
     Console.log("Creating stub file");
     fs.writeFileSync(outputPath, "", { flag: "w" });
 
-    Console.log("Generating stub for unencoded ideograph components");
-    rxjs.from(ids)
-        .pipe(
+    Console.log("Generating stub");
+    rxjs.concat(
+        rxjs.from(ids).pipe(
             skipWhile(x => !x.startsWith("#\t{01}")),
             takeWhile(x => x.startsWith("#\t{")),
-            map(x => /(?<placeholder>\{[0-9]+\})/.exec(x).groups.placeholder)
-        )
-        .subscribe(x =>
-            fs.writeFileSync(outputPath, x + "," + x + "," + "\r\n", {
-                encoding: "utf8",
-                flag: "as"
-            })
-        );
-
-    Console.log("Generating stub for unicode characters");
-    rxjs.from(ids)
-        .pipe(
+            map(x => /(?<placeholder>\{[0-9]+\})/.exec(x).groups.placeholder),
+            map(x => x + "," + x + "," + "\r\n")
+        ),
+        rxjs.from(ids).pipe(
             skipWhile(x => x.startsWith("#")),
             takeWhile(x => !x.startsWith("#")),
             map(x => x.split(/\t/)),
             map(x => ({ codepoint: x[0], literal: x[1], patterns: x.slice(2, x.length) })),
-            filter(x => x.patterns.every(y => y === "^" + x.literal + "$"))
+            filter(x => x.patterns.every(y => y === "^" + x.literal + "$")),
+            map(x => x.codepoint + "," + x.literal + "," + "\r\n")
         )
-        .subscribe(x =>
-            fs.writeFileSync(outputPath, x.codepoint + "," + x.literal + "," + "\r\n", {
+    ).subscribe(
+        x =>
+            fs.writeFileSync(outputPath, x, {
                 encoding: "utf8",
                 flag: "as"
-            })
-        );
+            }),
+        x => Console.error(x),
+        () => done()
+    );
 });
